2e606198550fe420aa4b5eabc98f15d1c77ff0d2
Ubokchi <105459234+Ubokchi@users.noreply.github.com>
2024-12-19 16:39:24 +0900

    Add files via upload

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import model.dao.UserDAO;
+import model.domain.User;
+import model.dto.UserDTO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UserDAO userDAO = new UserDAO();
+        User user = null;
+        try {
+            user = userDAO.findUser(email);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            response.sendRedirect("Main.jsp");
+        } else {
+            response.sendRedirect("LoginForm.jsp?error=true");
+        }
+    }
+}
